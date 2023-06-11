6a34f96184f31d9f1f69ac7fe37df7e57ec34df1
FedeCosta0 <86005791+FedeCosta0@users.noreply.github.com>
2023-06-11 21:48:53 +0200

    Update settings.py

--- PPM_Ecommerce_API/settings.py
+++ PPM_Ecommerce_API/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS")
+ALLOWED_HOSTS =["web-production-db80.up.railway.app"]
 
 # Application definition
 

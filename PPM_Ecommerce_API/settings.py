c66143a8b8fbb264d763671e6fc6078589b53de0
FedeCosta0 <86005791+FedeCosta0@users.noreply.github.com>
2023-06-11 22:13:54 +0200

    Update settings.py

--- PPM_Ecommerce_API/settings.py
+++ PPM_Ecommerce_API/settings.py
@@ -115,6 +115,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 

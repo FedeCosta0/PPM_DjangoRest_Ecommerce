7637dfbe7fbc1cfafb002aff483cfa7691a0680e
Federico Costantini <fedecosta11@gmail.com>
2023-06-11 14:04:47 +0200

    second commit

--- PPM_Ecommerce_API/settings.py
+++ PPM_Ecommerce_API/settings.py
@@ -14,8 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=0))
-# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS")
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -72,7 +72,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'ktNn8D3ndh3iUX3le7bw',
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
         'HOST': 'containers-us-west-108.railway.app',
         'PORT': '5821',
     }

b55830e15a4e0e2a50090656dce33c363c91525f
fedec0 <fedecosta11@gmail.com>
2023-06-15 19:46:37 +0200

    improved serializers

--- ecommerce_products/serializers.py
+++ ecommerce_products/serializers.py
@@ -32,25 +32,30 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class ProductCategorySerializer(serializers.ModelSerializer):
     products = serializers.PrimaryKeyRelatedField(many=True, read_only=False, queryset=Product.objects.all())
+
     class Meta:
         model = ProductCategory
         fields = [
             'name',
             'description',
+            'products',
         ]
 
 
 class ProductInventorySerializer(serializers.ModelSerializer):
     product = serializers.PrimaryKeyRelatedField(many=True, read_only=False, queryset=Product.objects.all())
+
     class Meta:
         model = ProductInventory
         fields = [
             'quantity',
+            'product',
         ]
 
 
 class DiscountSerializer(serializers.ModelSerializer):
     products = serializers.PrimaryKeyRelatedField(many=True, read_only=False, queryset=Product.objects.all())
+
     class Meta:
         model = Discount
         fields = [
@@ -58,6 +63,7 @@ class DiscountSerializer(serializers.ModelSerializer):
             'description',
             'discount_percent',
             'active',
+            'products',
         ]
 
 

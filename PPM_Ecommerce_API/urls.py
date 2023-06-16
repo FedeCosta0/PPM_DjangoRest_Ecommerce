c712266440833159c699d07658f53d76f80f91f7
fedec0 <fedecosta11@gmail.com>
2023-06-16 21:06:37 +0200

    added cart router

--- PPM_Ecommerce_API/urls.py
+++ PPM_Ecommerce_API/urls.py
@@ -5,13 +5,16 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from ecommerce_products.views import ProductViewSet, ProductInventoryViewSet, ProductCategoryViewSet, DiscountViewSet
 from ecommerce_users.views import UserViewSet
+from ecommerce_cart.views import ShoppingSessionViewSet, CartProductViewSet
 from core import views as core_views
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet, basename='products')
-router.register(r'productsinventory', ProductInventoryViewSet, basename='productsinventory')
-router.register(r'productscategory', ProductCategoryViewSet, basename='productscategory')
+router.register(r'products-inventory', ProductInventoryViewSet, basename='productsinventory')
+router.register(r'product-scategory', ProductCategoryViewSet, basename='productscategory')
 router.register(r'discount', DiscountViewSet, basename='discount')
+router.register(r'cart', ShoppingSessionViewSet, basename='cart')
+router.register(r'cart-product', CartProductViewSet, basename='cart-product')
 # router.register(r'order', ecommerce_views.OrderViewSet, basename='order') todo
 router.register(r'users', UserViewSet, basename='users')
 

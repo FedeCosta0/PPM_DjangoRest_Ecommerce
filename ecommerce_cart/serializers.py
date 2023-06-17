1facae6e3d014b9927fe7823f7fe1a3abe5c8de3
fedec0 <fedecosta11@gmail.com>
2023-06-17 12:18:21 +0200

    improved cart serializer

--- ecommerce_cart/serializers.py
+++ ecommerce_cart/serializers.py
@@ -4,11 +4,14 @@ from .models import ShoppingSession, CartProduct
 
 
 class ShoppingSessionSerializer(serializers.ModelSerializer):
+    cart_products = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
     class Meta:
         model = ShoppingSession
         fields = [
             'user',
             'total',
+            'cart_products'
         ]
 
 

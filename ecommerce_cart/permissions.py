fa2ac407f10eeceac1a642039ab5280711510457
fedec0 <fedecosta11@gmail.com>
2023-06-19 16:47:44 +0200

    code refactoring

--- ecommerce_cart/permissions.py
+++ ecommerce_cart/permissions.py
@@ -15,10 +15,7 @@ class ShoppingSessionPermission(permissions.BasePermission):
 
 class CartProductPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        if view.action == 'list':
-            return bool(request.user.is_authenticated)
-        elif view.action in ['create', 'retrieve', 'update', 'partial_update', 'destroy']:
-            return bool(request.user.is_authenticated)
+        return bool(request.user and request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         if view.action in ['retrieve', 'update', 'partial_update', 'destroy']:

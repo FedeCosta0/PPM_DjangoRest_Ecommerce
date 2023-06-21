316b0b786c781ee669eba9641cd6817041eff562
fedec0 <fedecosta11@gmail.com>
2023-06-21 18:21:40 +0200

    Reformatted code

--- ecommerce_users/views.py
+++ ecommerce_users/views.py
@@ -1,14 +1,14 @@
 from json import JSONDecodeError
 
+from django.contrib.auth import login
 from django.http import JsonResponse
-from rest_framework import viewsets, status
-from knox.auth import TokenAuthentication
 from knox import views as knox_views
+from knox.auth import TokenAuthentication
+from rest_framework import viewsets, status
+from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin, ListModelMixin
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import AllowAny
-from django.contrib.auth import login
 from rest_framework.response import Response
-from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin, ListModelMixin
 
 from ecommerce_users.models import CustomUser
 from .permissions import UserPermission
@@ -38,14 +38,15 @@ class UserViewSet(RetrieveModelMixin, UpdateModelMixin, ListModelMixin, viewsets
         try:
             data = JSONParser().parse(request)
 
-            serializer = UserRegistrationSerializer(data=data)
+            serializer = self.serializer_class(data=data)
 
             if serializer.is_valid(raise_exception=True):
                 print(serializer.validated_data)
                 print(repr(serializer.validated_data))
                 validated_data = serializer.validated_data
                 user = CustomUser.objects.create(email=validated_data['email'], password=validated_data['password'],
-                                                 first_name=validated_data['first_name'], last_name=validated_data['last_name'])
+                                                 first_name=validated_data['first_name'],
+                                                 last_name=validated_data['last_name'])
                 return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -58,16 +59,15 @@ class LoginAPIView(knox_views.LoginView):
     serializer_class = UserLoginSerializer
 
     def post(self, request, format=None):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            user = serializer.validated_data['user']
-            login(request, user)
-            response = super().post(request, format=None)
+        try:
+            data = JSONParser().parse(request)
+            serializer = self.serializer_class(data=data)
 
-        else:
-            return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response(response.data, status=status.HTTP_200_OK)
-
-
-
+            if serializer.is_valid(raise_exception=True):
+                user = serializer.validated_data['user']
+                login(request, user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        except JSONDecodeError:
+            return JsonResponse({"result": "error", "message": "Json decoding error"}, status=400)

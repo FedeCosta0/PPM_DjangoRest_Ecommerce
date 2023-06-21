69fac0226275c8c2a52c6bf9e22e4346c900c483
fedec0 <fedecosta11@gmail.com>
2023-06-21 16:30:04 +0200

    Reformatted code

--- ecommerce_cart/serializers.py
+++ ecommerce_cart/serializers.py
@@ -3,8 +3,18 @@ from rest_framework import serializers
 from .models import ShoppingSession, CartProduct
 
 
+class CartProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartProduct
+        fields = [
+            'product',
+            'quantity'
+        ]
+
+
 class ShoppingSessionSerializer(serializers.ModelSerializer):
-    cart_products = serializers.StringRelatedField(many=True, read_only=True)
+    cart_products = CartProductSerializer(many=True, read_only=True)
+    user = serializers.ReadOnlyField(source='user.email')
 
     class Meta:
         model = ShoppingSession
@@ -12,14 +22,4 @@ class ShoppingSessionSerializer(serializers.ModelSerializer):
             'user',
             'total',
             'cart_products'
-        ]
-
-
-class CartProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CartProduct
-        fields = [
-            'shopping_session',
-            'product',
-            'quantity'
         ]

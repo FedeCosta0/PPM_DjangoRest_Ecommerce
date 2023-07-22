65a82a4a63872408a1d6f44c2792022618daf2ce
fedec0 <fedecosta11@gmail.com>
2023-07-22 12:16:49 +0200

    Reformatted Code

--- PPM_Ecommerce_API/wsgi.py
+++ PPM_Ecommerce_API/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PPM_Ecommerce_API_DEV.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PPM_Ecommerce_API.settings')
 
 application = get_wsgi_application()
